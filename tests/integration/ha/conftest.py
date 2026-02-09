08940ab26094b0eed6a36f9039c986a87144faaa
akram09 <akram.boutouchent@canonical.com>
2026-02-09 11:47:38 +0100

    patch: Add conftests

--- tests/integration/ha/conftest.py
+++ tests/integration/ha/conftest.py
@@ -9,7 +9,7 @@ import pytest
 from pytest_operator.plugin import OpsTest
 
 from tests.integration.conftest import APP_NAME
-from tests.integration.ha.continuous_writes import ContinuousWrites
+from tests.integration.ha.continuous_writes import ContinuousWrites, ReplicationMode
 from tests.integration.helpers import app_name
 
 
@@ -40,6 +40,24 @@ async def c_writes(ops_test: OpsTest):
 async def c_writes_runner(ops_test: OpsTest, c_writes: ContinuousWrites):
     """Starts continuous write operations and clears writes at the end of the test."""
     await c_writes.start()
+    yield
+    await c_writes.clear()
+    logger.info("\n\n\n\nThe writes have been cleared.\n\n\n\n")
+
+
+@pytest.fixture(scope="function")
+async def c_0_repl_writes_runner(ops_test: OpsTest, c_writes: ContinuousWrites):
+    """Starts continuous write operations and clears writes at the end of the test."""
+    await c_writes.start(repl_mode=ReplicationMode.WITH_AT_LEAST_0_REPL)
+    yield
+    await c_writes.clear()
+    logger.info("\n\n\n\nThe writes have been cleared.\n\n\n\n")
+
+
+@pytest.fixture(scope="function")
+async def c_balanced_writes_runner(ops_test: OpsTest, c_writes: ContinuousWrites):
+    """Same as previous runner, but starts continuous writes on cluster wide replicated index."""
+    await c_writes.start(repl_mode=ReplicationMode.WITH_AT_LEAST_1_REPL)
     yield
     await c_writes.clear()
     logger.info("\n\n\n\nThe writes have been cleared.\n\n\n\n")
